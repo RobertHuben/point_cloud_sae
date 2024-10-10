74149dd351733e788ca91af08226bc00e3e008de
Robert <rvhuben@gmail.com>
2024-10-10 17:13:21 -0400

    moderate refactoring

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import torch
 import random
 import time
 
-from point_cloud_sae import TopkSAE, SAEAnthropic, graph_point_cloud_results, graph_point_cloud_results_unified
+from point_cloud_sae import TopkSAE, SAEAnthropic
 from point_cloud_datasets import PointCloudDataset, generate_point_cloud_blobs, generate_point_cloud_lollipops 
-
+from analysis import graph_point_cloud_results, graph_point_cloud_results_unified, graph_reconstruction_errors
 
 if __name__=="__main__":
     mode='blobs'
@@ -13,11 +13,13 @@ if __name__=="__main__":
     train_size=100
     test_size=1000
     num_epochs=100000//train_size
-    num_features = 3 if mode == "blobs" else 5
+    num_features = 5
+    cloud_scale_factor=10
 
     randomize_class_weights=False
     if randomize_class_weights:
-        class_weights=[random.choice([1,4]) for _ in range(num_features)]
+        class_weights=[random.choice([1,2]) for _ in range(num_features)]
+        print(f"Randomized class weights are: {class_weights}")
     else:
         class_weights=[1 for _ in range(num_features)]
 
@@ -30,14 +32,13 @@ if __name__=="__main__":
         train_dataset=PointCloudDataset(generate_point_cloud_lollipops(train_size, seed=2, class_weights=class_weights))
         test_dataset=PointCloudDataset(generate_point_cloud_lollipops(test_size, seed=3, class_weights=class_weights))
 
-    sae=SAEAnthropic(anchors, num_features=num_features, l1_sparsity_coefficient=2, cloud_scale_factor=5)
-    # sae=TopkSAE(anchors, num_features=num_features, k=1, use_relu=True, l1_sparsity_coefficient=0, cloud_scale_factor=5)
-    t_start=time.time()
-    sae.train_sae(train_dataset=train_dataset, eval_dataset=test_dataset, learning_rate=1e-4, num_epochs=num_epochs, report_every_n_data=5000, batch_size=100)
-    t_end=time.time()
-    print(f"{t_end-t_start} seconds")
+    for seed in range(5):
+        torch.random.manual_seed(seed)
+        # sae=SAEAnthropic(anchors, num_features=num_features, l1_sparsity_coefficient=2, cloud_scale_factor=cloud_scale_factor)
+        sae=TopkSAE(anchors, num_features=num_features, k=1, use_relu=True, l1_sparsity_coefficient=0, cloud_scale_factor=cloud_scale_factor)
+        sae.train_sae(train_dataset=train_dataset, eval_dataset=test_dataset, learning_rate=1e-4, num_epochs=num_epochs, report_every_n_data=10000, reinitialize_every_n_data=20000, batch_size=100)
     torch.save(sae, "trained_saes/point_cloud_sae.pkl")
     graph_point_cloud_results(test_dataset, anchors=anchors)
+    graph_reconstruction_errors(test_dataset)
     graph_point_cloud_results_unified(test_dataset, anchors=anchors, anchor_details='encoder', save_name='all_features_encoder')
     graph_point_cloud_results_unified(test_dataset, anchors=anchors, anchor_details='decoder', save_name='all_features_decoder')
-    print(f"Class weights are: {class_weights}")
