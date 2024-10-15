6f8f5e66ff93c535669a815ebdd75d6b38f613f9
Robert <rvhuben@gmail.com>
2024-10-15 11:26:47 -0400

    rearranged and unified several methods

--- point_cloud_sae.py
+++ point_cloud_sae.py
@@ -24,6 +24,7 @@ class SAETemplate(torch.nn.Module, ABC):
         self.encoder, self.encoder_bias, self.decoder, self.decoder_bias=self.create_linear_encoder_decoder(decoder_initialization_scale)
         self.num_data_trained_on=0
         self.ghost_loss_style=ghost_loss_style
+        self.num_data_since_last_activation=torch.zeros((num_features))
 
     def catenate_outputs_on_dataset(self, dataset:PointCloudDataset, batch_size=8, include_loss=False):
         '''
@@ -57,6 +58,9 @@ class SAETemplate(torch.nn.Module, ABC):
         return sparsity_per_entry
 
     def find_dead_features(self, hidden_layers, eps=1e-6):
+        '''
+        reutrns a tensor of shape (num_features), which is True if the feature is dead, and False otherwise
+        '''
         active_features=hidden_layers>eps
         dead_features=torch.all(torch.flatten(~ active_features, end_dim=-2), dim=0)
         return dead_features
@@ -69,6 +73,7 @@ class SAETemplate(torch.nn.Module, ABC):
         cluster_assignments=hidden_layers.argmax(dim=1)
         return eval_dataset.compute_entropy_of_clustering(cluster_assignments)
 
+    @torch.inference_mode()
     def print_evaluation(self, train_loss, eval_dataset:PointCloudDataset, step_number="N/A"):
         losses, residual_streams, hidden_layers, reconstructed_residual_streams=self.catenate_outputs_on_dataset(eval_dataset, include_loss=True)
         test_loss=losses.mean()
@@ -117,8 +122,28 @@ class SAETemplate(torch.nn.Module, ABC):
             self.print_evaluation(train_loss=loss, eval_dataset=eval_dataset, step_number="Omega")
         self.eval()
 
+    def forward(self, residual_stream, compute_loss=False):
+        '''
+        takes the trimmed residual stream of a language model (as produced by run_gpt_and_trim) and runs the SAE
+        must return a tuple (loss, residual_stream, hidden_layer, reconstructed_residual_stream)
+        residual_stream is shape (B, W, D), where B is batch size, W is (trimmed) window length, and D is the dimension of the model:
+            - residual_stream is unchanged, of size (B, W, D)
+            - hidden_layer is of shape (B, W, D') where D' is the size of the hidden layer
+            - reconstructed_residual_stream is shape (B, W, D) 
+        '''
+        normalized_encoder = F.normalize(self.encoder, p=2, dim=1) #normalize columns
+        normalized_decoder = F.normalize(self.decoder, p=2, dim=1) #normalize columns
+        hidden_layer=self.activation_function((residual_stream - self.decoder_bias) @ normalized_encoder + self.encoder_bias)
+        reconstructed_residual_stream=hidden_layer @ normalized_decoder + self.decoder_bias
+        loss= self.loss_function(residual_stream, hidden_layer, reconstructed_residual_stream) if compute_loss else None
+        return loss, residual_stream, hidden_layer, reconstructed_residual_stream
+
     @abstractmethod
-    def forward(self, embedded_points, compute_loss=False):
+    def activation_function(encoder_output):
+        pass
+
+    @abstractmethod
+    def loss_function(encoder_output):
         pass
 
     def create_linear_encoder_decoder(self, decoder_initialization_scale):
@@ -152,13 +177,16 @@ class SAETemplate(torch.nn.Module, ABC):
     def ghost_loss(self,residual_streams, hidden_layers, reconstructed_residual_streams):
         dead_features=self.find_dead_features(hidden_layers)
         errors=residual_streams-reconstructed_residual_streams
-        error_magnitudes=errors.norm(dim=-1)**2
+        error_magnitudes=errors.norm(dim=-1)
         error_weighted_residual_stream=((residual_streams*error_magnitudes.unsqueeze(1)).sum(dim=0))/error_magnitudes.sum()
         error_weighted_error_direction=((errors*error_magnitudes.unsqueeze(1)).sum(dim=0))/error_magnitudes.sum()
         raw_encoder_ghost_loss=-1*error_weighted_residual_stream@(self.encoder *dead_features)
+        # encoder_ghost_loss=torch.nn.functional.relu(raw_encoder_ghost_loss).mean()
         encoder_ghost_loss=torch.nn.functional.softplus(raw_encoder_ghost_loss).mean()
         raw_decoder_ghost_loss=-1*error_weighted_error_direction@(self.decoder.T*dead_features)
+        # decoder_ghost_loss=torch.nn.functional.relu(raw_decoder_ghost_loss).mean()
         decoder_ghost_loss=torch.nn.functional.softplus(raw_decoder_ghost_loss).mean()
+        # decoder_ghost_loss=0
         ghost_loss=(encoder_ghost_loss+decoder_ghost_loss)
         return ghost_loss
 
@@ -195,14 +223,15 @@ class SAEAnthropic(SAETemplate):
         self.l1_sparsity_coefficient=l1_sparsity_coefficient
         self.ghost_loss_coefficient=ghost_loss_coefficient
 
-    def forward(self, embedded_points, compute_loss=False):
-        hidden_layer=self.activation_function(embedded_points @ self.encoder + self.encoder_bias)
-        reconstructed_embedded_points=hidden_layer @ self.decoder + self.decoder_bias
-        if compute_loss:
-            loss = self.loss_function(embedded_points, hidden_layer, reconstructed_embedded_points)
-        else:
-            loss = None
-        return loss, embedded_points, hidden_layer, reconstructed_embedded_points
+    # def forward(self, embedded_points, compute_loss=False):
+    #     hidden_layer=self.activation_function(embedded_points @ self.encoder + self.encoder_bias)
+    #     reconstructed_embedded_points=hidden_layer @ self.decoder + self.decoder_bias
+    #     if compute_loss:
+    #         loss = self.loss_function(embedded_points, hidden_layer, reconstructed_embedded_points)
+    #     else:
+    #         loss = None
+    #     self.update_
+    #     return loss, embedded_points, hidden_layer, reconstructed_embedded_points
     
     def loss_function(self, residual_stream, hidden_layer, reconstructed_residual_stream):
         reconstruction_loss=self.reconstruction_error(residual_stream, reconstructed_residual_stream)
@@ -230,51 +259,43 @@ class TopkSAE(SAETemplate):
         self.l1_sparsity_coefficient=l1_sparsity_coefficient
         self.ghost_loss_coefficient=ghost_loss_coefficient
 
-
     def activation_function(self, encoder_output):
         if self.use_relu:
             activations = torch.relu(encoder_output)
         else:
             activations=encoder_output
-        highest_indices=activations.argmax(dim=1)
-        activations_mask=torch.zeros(activations.shape).scatter_(dim=1,index=highest_indices.unsqueeze(1), value=1)
+        with torch.no_grad():
+            highest_indices=activations.argmax(dim=1)
+            activations_mask=torch.zeros(activations.shape).scatter_(dim=1,index=highest_indices.unsqueeze(1), value=1)
         return activations*activations_mask
 
-    def forward(self, residual_stream, compute_loss=False):
-        '''
-        takes the trimmed residual stream of a language model (as produced by run_gpt_and_trim) and runs the SAE
-        must return a tuple (loss, residual_stream, hidden_layer, reconstructed_residual_stream)
-        residual_stream is shape (B, W, D), where B is batch size, W is (trimmed) window length, and D is the dimension of the model:
-            - residual_stream is unchanged, of size (B, W, D)
-            - hidden_layer is of shape (B, W, D') where D' is the size of the hidden layer
-            - reconstructed_residual_stream is shape (B, W, D) 
-        '''
-        normalized_encoder = F.normalize(self.encoder, p=2, dim=1) #normalize columns
-        # normalized_decoder = normalized_encoder.T
-        normalized_decoder = F.normalize(self.decoder, p=2, dim=1) #normalize columns
-        hidden_layer=self.activation_function((residual_stream - self.decoder_bias) @ normalized_encoder + self.encoder_bias)
-        reconstructed_residual_stream=hidden_layer @ normalized_decoder + self.decoder_bias
-        loss= self.loss_function(residual_stream, hidden_layer, reconstructed_residual_stream) if compute_loss else None
-        return loss, residual_stream, hidden_layer, reconstructed_residual_stream
+    # def forward(self, residual_stream, compute_loss=False):
+    #     '''
+    #     takes the trimmed residual stream of a language model (as produced by run_gpt_and_trim) and runs the SAE
+    #     must return a tuple (loss, residual_stream, hidden_layer, reconstructed_residual_stream)
+    #     residual_stream is shape (B, W, D), where B is batch size, W is (trimmed) window length, and D is the dimension of the model:
+    #         - residual_stream is unchanged, of size (B, W, D)
+    #         - hidden_layer is of shape (B, W, D') where D' is the size of the hidden layer
+    #         - reconstructed_residual_stream is shape (B, W, D) 
+    #     '''
+    #     normalized_encoder = F.normalize(self.encoder, p=2, dim=1) #normalize columns
+    #     # normalized_decoder = normalized_encoder.T
+    #     normalized_decoder = F.normalize(self.decoder, p=2, dim=1) #normalize columns
+    #     hidden_layer=self.activation_function((residual_stream - self.decoder_bias) @ normalized_encoder + self.encoder_bias)
+    #     reconstructed_residual_stream=hidden_layer @ normalized_decoder + self.decoder_bias
+    #     loss= self.loss_function(residual_stream, hidden_layer, reconstructed_residual_stream) if compute_loss else None
+    #     return loss, residual_stream, hidden_layer, reconstructed_residual_stream
 
     def report_model_specific_features(self):
         return [f"k (sparsity): {self.k}", f"Leakiness: {self.leakiness}"]
 
     def loss_function(self, residual_stream, hidden_layer, reconstructed_residual_stream):
         reconstruction_loss=self.reconstruction_error(residual_stream, reconstructed_residual_stream)
-        sparsity_loss= self.sparsity_loss_function(residual_stream)*self.l1_sparsity_coefficient
+        sparsity_loss= self.sparsity_loss_function(hidden_layer)*self.l1_sparsity_coefficient
         if self.ghost_loss_style=="new":
             ghost_loss=self.ghost_loss(residual_stream, hidden_layer, reconstructed_residual_stream)*self.ghost_loss_coefficient
         elif self.ghost_loss_style=="anthropic":
             ghost_loss=self.ghost_loss_anthropic(residual_stream, hidden_layer, reconstructed_residual_stream)
         total_loss=reconstruction_loss+sparsity_loss+ghost_loss
         return total_loss
-
-    def sparsity_loss_function(self, residual_stream):
-        normalized_encoder = F.normalize(self.encoder, p=2, dim=1) #normalize columns
-        normalized_decoder = F.normalize(self.decoder, p=2, dim=1) #normalize columns
-        pre_activation_hidden_layer=(residual_stream - self.decoder_bias) @ normalized_encoder + self.encoder_bias
-        decoder_column_norms=self.decoder.norm(dim=1)
-        return torch.mean(pre_activation_hidden_layer*decoder_column_norms)
-
 
