c58ba9432ddccfbdff4fd05c90f65ad36e1e961d
Robert <rvhuben@gmail.com>
2024-10-28 10:39:15 -0400

    added code for adjoint classification

--- point_cloud_sae.py
+++ point_cloud_sae.py
@@ -31,7 +31,6 @@ class SAETemplate(torch.nn.Module, ABC):
         self.l1_sparsity_coefficient=l1_sparsity_coefficient
         self.ghost_loss_coefficient=ghost_loss_coefficient
 
-
     def catenate_outputs_on_dataset(self, dataset:PointCloudDataset, batch_size=8, include_loss=False):
         '''
         runs the model on the entire dataset, one batch at a time, catenating the outputs
@@ -79,6 +78,10 @@ class SAETemplate(torch.nn.Module, ABC):
         cluster_assignments=hidden_layers.argmax(dim=1)
         return eval_dataset.compute_entropy_of_clustering(cluster_assignments)
 
+    def compute_adjoint_classification_entropy(self, eval_dataset:PointCloudDataset):
+        cluster_assignments=self.adjoint_classify(eval_dataset)
+        return eval_dataset.compute_entropy_of_clustering(cluster_assignments)
+
     @torch.inference_mode()
     def report_evaluation(self, train_loss, eval_dataset:PointCloudDataset, step_number="N/A", write_location=None):
         losses, residual_streams, hidden_layers, reconstructed_residual_streams=self.catenate_outputs_on_dataset(eval_dataset, include_loss=True)
@@ -86,8 +89,9 @@ class SAETemplate(torch.nn.Module, ABC):
         l0_sparsity=self.compute_l0_sparsity(hidden_layers)
         dead_features=self.count_dead_features(hidden_layers)
         entropy=self.compute_classification_entropy(hidden_layers, eval_dataset)
+        adjoint_entropy=self.compute_adjoint_classification_entropy(eval_dataset)
         if write_location:
-            write_location.update(self, train_loss, test_loss, dead_features, entropy)
+            write_location.update(self, train_loss, test_loss, dead_features, entropy, adjoint_entropy)
         else:
             print_message=f"Train loss, test loss, l0 sparsity, dead features, entropy after training on {self.num_data_trained_on} datapoints: {train_loss.item():.2f}, {test_loss:.2f}, {l0_sparsity:.1f}, {dead_features:.0f}, {entropy:.2f}"
             tqdm.write(print_message)
@@ -229,6 +233,15 @@ class SAETemplate(torch.nn.Module, ABC):
         cluster_assignments=hidden_layers.argmax(dim=1)
         return cluster_assignments
 
+    def adjoint_classify(self, dataset:PointCloudDataset):
+        '''
+            uses the decoder to find the classifications
+        '''
+        embedded_points=embed_point_cloud(dataset.points, anchors=self.anchor_points, scale_factor=self.cloud_scale_factor)
+        decoder_point_activations=embedded_points @self.decoder.T
+        cluster_assignments=decoder_point_activations.argmax(dim=1)
+        return cluster_assignments
+
 class SAEAnthropic(SAETemplate):
 
     def __init__(self, anchor_points:torch.tensor, num_features:int, l1_sparsity_coefficient:float, ghost_loss_coefficient:float=.1, decoder_initialization_scale=0.1, cloud_scale_factor=1, ghost_loss_style="new"):
@@ -240,16 +253,7 @@ class SAEAnthropic(SAETemplate):
                          l1_sparsity_coefficient=l1_sparsity_coefficient,
                          ghost_loss_coefficient=ghost_loss_coefficient
                          )
-    # def forward(self, embedded_points, compute_loss=False):
-    #     hidden_layer=self.activation_function(embedded_points @ self.encoder + self.encoder_bias)
-    #     reconstructed_embedded_points=hidden_layer @ self.decoder + self.decoder_bias
-    #     if compute_loss:
-    #         loss = self.loss_function(embedded_points, hidden_layer, reconstructed_embedded_points)
-    #     else:
-    #         loss = None
-    #     self.update_
-    #     return loss, embedded_points, hidden_layer, reconstructed_embedded_points
-    
+
     def loss_function(self, residual_stream, hidden_layer, reconstructed_residual_stream):
         reconstruction_loss=self.reconstruction_error(residual_stream, reconstructed_residual_stream)
         sparsity_loss= self.sparsity_loss_function(hidden_layer)*self.l1_sparsity_coefficient
@@ -290,23 +294,6 @@ class TopkSAE(SAETemplate):
             activations_mask=torch.zeros(activations.shape).scatter_(dim=1,index=highest_indices.unsqueeze(1), value=1)
         return activations*activations_mask
 
-    # def forward(self, residual_stream, compute_loss=False):
-    #     '''
-    #     takes the trimmed residual stream of a language model (as produced by run_gpt_and_trim) and runs the SAE
-    #     must return a tuple (loss, residual_stream, hidden_layer, reconstructed_residual_stream)
-    #     residual_stream is shape (B, W, D), where B is batch size, W is (trimmed) window length, and D is the dimension of the model:
-    #         - residual_stream is unchanged, of size (B, W, D)
-    #         - hidden_layer is of shape (B, W, D') where D' is the size of the hidden layer
-    #         - reconstructed_residual_stream is shape (B, W, D) 
-    #     '''
-    #     normalized_encoder = F.normalize(self.encoder, p=2, dim=1) #normalize columns
-    #     # normalized_decoder = normalized_encoder.T
-    #     normalized_decoder = F.normalize(self.decoder, p=2, dim=1) #normalize columns
-    #     hidden_layer=self.activation_function((residual_stream - self.decoder_bias) @ normalized_encoder + self.encoder_bias)
-    #     reconstructed_residual_stream=hidden_layer @ normalized_decoder + self.decoder_bias
-    #     loss= self.loss_function(residual_stream, hidden_layer, reconstructed_residual_stream) if compute_loss else None
-    #     return loss, residual_stream, hidden_layer, reconstructed_residual_stream
-
     def report_model_specific_features(self):
         return [f"k (sparsity): {self.k}", f"Leakiness: {self.leakiness}"]
 
@@ -328,13 +315,15 @@ class TrainingLog:
         self.test_losses=[]
         self.dead_feature_counts=[]
         self.entropies=[]
+        self.adjoint_entropies=[]
 
-    def update(self, sae:SAETemplate, train_loss, test_loss, dead_features, entropy):
+    def update(self, sae:SAETemplate, train_loss, test_loss, dead_features, entropy, adjoint_entropy):
         self.num_data_seen.append(sae.num_data_trained_on)
         self.train_losses.append(train_loss)
         self.test_losses.append(test_loss)
         self.dead_feature_counts.append(dead_features)
         self.entropies.append(entropy)
+        self.adjoint_entropies.append(adjoint_entropy)
 
     def export_to_tensors(self):
         num_data_seen=torch.tensor(self.num_data_seen)
@@ -342,4 +331,5 @@ class TrainingLog:
         test_losses=torch.tensor(self.test_losses)
         dead_feature_counts=torch.tensor(self.dead_feature_counts)
         entropies=torch.tensor(self.entropies)
-        return num_data_seen, train_losses, test_losses, dead_feature_counts, entropies
+        adjoint_entropies=torch.tensor(self.adjoint_entropies)
+        return num_data_seen, train_losses, test_losses, dead_feature_counts, entropies, adjoint_entropies
